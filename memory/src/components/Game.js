100ad86737c8d9fb202a850ca963bc7e77c7da12
Lilu <KHussell@gmail.com>
2019-07-19 14:18:01 -0400

    emptying div of pictures works, getting multiple pictures

--- memory/src/components/Game.js
+++ memory/src/components/Game.js
@@ -4,16 +4,20 @@ import Character from "./Character"
 
 
 class Game extends React.Component {
-  
+    emptyDiv =() => {
+        let emptyThisDiv = document.getElementsByClassName("needsEmpty")
+        emptyThisDiv.innerHTML=""
+    }
 
     render() {
+        this.emptyDiv()
         let shuffled = this.props.characters
             .map((a) => ({ sort: Math.random(), value: a }))
             .sort((a, b) => a.sort - b.sort)
             .map((a) => a.value)
 
         return (
-            <div className="container">
+            <div className="container needsEmpty">
                 <div style={{ marginTop: 100 }} className="row">
                     {shuffled.map(char => {
                         return (<Character handleClick={this.props.handleClick} key={char.id} id={char.id} image={char.image} clicked={char.clicked} />)
